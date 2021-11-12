775eccdb22510c3273458796f64277a0de91a0df
hqguo1116 <hnxcguohaoqiang2010@live.cn>
2021-11-11 23:20:08 -0600

    Find Peak Element

---
+++ Binary_search/162.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int i = 0;
+        int j = nums.size()-1;
+        int mid;
+        while (i<j){
+            mid = (i+j)/2;
+            if (nums[mid]>nums[mid+1]){
+                j = mid;
+            }else{
+                i = mid+1;
+            }
+        }
+        return i;
+    }
+};
